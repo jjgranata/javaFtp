2255544d98ff04311feea623c06a7240ff7a86cb
Zach <zach.yun1@gmail.com>
2016-11-29 01:58:50 -0800

    XOR cipher

---
+++ src/XOR.java
@@ -0,0 +1,27 @@
+package cs380;
+
+public class XOR 
+{
+	
+	private static StringBuilder xorMessage(String in) 
+	{
+		
+		char[] key = {'A'};
+		
+		StringBuilder out = new StringBuilder();
+		
+		for(int i = 0; i < in.length(); i++) {
+			out.append((char) (in.charAt(i) ^ key[i % key.length]));
+		}
+ 		
+		return out;
+	}
+	
+	public static void main(String[] args) {
+		String original = XOR.xorMessage("MessageToEncrypt").toString();
+		System.out.println(original);
+		String decrypted = XOR.xorMessage(original).toString();
+		System.out.println(decrypted);
+		
+	}
+}
